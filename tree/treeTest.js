25766ea5f2baec086a5f74955df83c4ecbc87101
Emile Fokkema <emile.fokkema@t-mobile.nl>
2017-04-24 09:59:02 +0200

    TreeNode done

--- tree/treeTest.js
+++ tree/treeTest.js
@@ -53,5 +53,47 @@
 		var node = TreeNodeBase.create();
 		node = node.expandUniverse();
 		this.assert(node.level == 4, "level should be 4");
-	})
+	});
+
+	test("oneGenTest",function(){
+		var node = new TreeNode(true);
+		var aliveInNext = node.oneGen(0b100111).alive; //three south neighbors are alive
+		this.assert(aliveInNext);
+
+		node = new TreeNode(false);
+		aliveInNext = node.oneGen(0b100111).alive; //three south neighbors are alive
+		this.assert(aliveInNext);
+
+		node = new TreeNode(false);
+		aliveInNext = node.oneGen(0b11).alive; //two south neighbors are alive
+		this.assert(!aliveInNext);
+
+		node = new TreeNode(true);
+		aliveInNext = node.oneGen(0b100001).alive; //one south neighbor is alive
+		this.assert(!aliveInNext);
+	});
+
+	test("slowSimulationTest",function(){
+		var node = TreeNodeBase.create().nw;
+		node = node.setBit(-2,-2).setBit(-1,-2).setBit(0,-2).setBit(1,-2); //a 4x4 with the top four alive
+		var resultNode = node.slowSimulation(); //should be a 2x2 with the top two alive
+		this.assert(resultNode.population == 2);
+
+	});
+
+	test("nextGenerationTestEmpty",function(){
+		var node = TreeNodeBase.create();
+		var next = node.nextGeneration();
+		this.assert(next.level == 2, "level should be 2");
+		this.assert(next.population == 0, "population should be 0");
+	});
+
+	test("nextGenerationTestNonEmpty",function(){
+		var node = TreeNodeBase.create().setBit(0,0).setBit(0,-1).setBit(0,1); //a blinker in its vertical state
+		var next = node.nextGeneration();
+		this.assert(next.population == 3);
+		this.assert(next.getBit(0,0) == 1);
+		this.assert(next.getBit(1,0) == 1);
+		this.assert(next.getBit(-1,0) == 1);
+	});
 })();
--- tree/treenode.js
+++ tree/treenode.js
@@ -3,7 +3,83 @@
 		TreeNodeBase.apply(this,arguments);
 	};
 
-	TreeNode.prototype = Object.create(TreeNodeBase.prototype, {});
+	TreeNode.prototype = Object.create(TreeNodeBase.prototype, {
+		oneGen: {
+			value:function(bitMask){
+				if(bitMask == 0){
+					return this.create(false);
+				}
+				var self = (bitMask >> 5) & 1 ;
+				bitMask &= 0x757 ;
+				var neighborCount = 0;
+				while (bitMask != 0) {
+				   neighborCount++ ;
+				   bitMask &= bitMask - 1 ;
+				}
+				if (neighborCount == 3 || (neighborCount == 2 && self != 0)){
+				   return this.create(true) ;
+				}
+				else{
+				   return this.create(false) ;
+				}
+			}
+		},
+		slowSimulation:{
+			value:function(){
+				var allBits = 0;
+				for(var y=-2;y<2;y++){
+					for(var x=-2;x<2;x++){
+						allBits = (allBits << 1) + this.getBit(x, y) ;
+					}
+				}
+				return this.create(this.oneGen(allBits>>5), this.oneGen(allBits>>4),
+				              this.oneGen(allBits>>1), this.oneGen(allBits)) ;
+			}
+		},
+		centeredSubnode:{
+			value:function(){
+				return this.create(this.nw.se, this.ne.sw, this.sw.ne, this.se.nw) ;
+			}
+		},
+		centeredHorizontal:{
+			value:function(w, e){
+				return this.create(w.ne.se, e.nw.sw, w.se.ne, e.sw.nw) ;
+			}
+		},
+		centeredVertical:{
+			value:function(n, s){
+				return this.create(n.sw.se, n.se.sw, s.nw.ne, s.ne.nw) ;
+			}
+		},
+		centeredSubSubnode:{
+			value:function(){
+				return this.create(this.nw.se.se, this.ne.sw.sw, this.sw.ne.ne, this.se.nw.nw) ;
+			}
+		},
+		nextGeneration:{
+			value:function(){
+				if(this.population == 0){
+					return this.nw;
+				}
+				if(this.level == 2){
+					return this.slowSimulation();
+				}
+				var n00 = this.nw.centeredSubnode(),
+					n01 = this.centeredHorizontal(this.nw, this.ne),
+					n02 = this.ne.centeredSubnode(),
+					n10 = this.centeredVertical(this.nw, this.sw),
+					n11 = this.centeredSubSubnode(),
+					n12 = this.centeredVertical(this.ne, this.se),
+					n20 = this.sw.centeredSubnode(),
+					n21 = this.centeredHorizontal(this.sw, this.se),
+					n22 = this.se.centeredSubnode();
+				return this.create(this.create(n00, n01, n10, n11).nextGeneration(),
+				              this.create(n01, n02, n11, n12).nextGeneration(),
+				              this.create(n10, n11, n20, n21).nextGeneration(),
+				              this.create(n11, n12, n21, n22).nextGeneration()) ;
+			}
+		}
+	});
 
 	window.TreeNode = TreeNode;
 })();
