760bdeb47f338abb405b0d812c0a96f92c6b9f09
Emile Fokkema <emilefokkema@gmail.com>
2017-05-03 22:33:42 +0200

    only memoizedtreenode is used, so only memoizedtreenode is tested

--- tree/treeTest.js
+++ tree/treeTest.js
@@ -25,12 +25,12 @@
 	};
 
 	test("nodeTest1",function(){
-		var baseNw = new TreeNode(true);
-		var baseNe = new TreeNode(false);
-		var baseSw = new TreeNode(false);
-		var baseSe = new TreeNode(true);
+		var baseNw = new MemoizedTreeNode(true);
+		var baseNe = new MemoizedTreeNode(false);
+		var baseSw = new MemoizedTreeNode(false);
+		var baseSe = new MemoizedTreeNode(true);
 
-		var combination = new TreeNode(baseNw, baseNe, baseSw, baseSe);
+		var combination = new MemoizedTreeNode(baseNw, baseNe, baseSw, baseSe);
 		this.assert(combination.population == 2,"population should be 2");
 		this.assert(combination.alive,"should be alive");
 		this.assert(combination.level == 1, "level should be 1");
@@ -38,12 +38,12 @@
 	});
 
 	test("createTest",function(){
-		var created = TreeNodeBase.create();
+		var created = MemoizedTreeNode.create();
 		this.assert(created.level == 3);
 	});
 
 	test("setBitTest",function(){
-		var node = TreeNodeBase.create();
+		var node = MemoizedTreeNode.create();
 		node = node.setBit(0,0);
 		node = node.setBit(1,0);
 		this.assert(node.population == 2, "population should be 2");
@@ -52,37 +52,37 @@
 	});
 
 	test("containTest",function(){
-		var node = TreeNodeBase.create();
+		var node = MemoizedTreeNode.create();
 		this.assert(node.canContain(1,1));
 		this.assert(!node.canContain(35,35));
 	});
 	
 	test("expandUniverseTest",function(){
-		var node = TreeNodeBase.create();
+		var node = MemoizedTreeNode.create();
 		node = node.expandUniverse();
 		this.assert(node.level == 4, "level should be 4");
 	});
 
 	test("oneGenTest",function(){
-		var node = new TreeNode(true);
+		var node = new MemoizedTreeNode(true);
 		var aliveInNext = node.oneGen(0b100111).alive; //three south neighbors are alive
 		this.assert(aliveInNext);
 
-		node = new TreeNode(false);
+		node = new MemoizedTreeNode(false);
 		aliveInNext = node.oneGen(0b100111).alive; //three south neighbors are alive
 		this.assert(aliveInNext);
 
-		node = new TreeNode(false);
+		node = new MemoizedTreeNode(false);
 		aliveInNext = node.oneGen(0b11).alive; //two south neighbors are alive
 		this.assert(!aliveInNext);
 
-		node = new TreeNode(true);
+		node = new MemoizedTreeNode(true);
 		aliveInNext = node.oneGen(0b100001).alive; //one south neighbor is alive
 		this.assert(!aliveInNext);
 	});
 
 	test("slowSimulationTest",function(){
-		var node = TreeNodeBase.create().nw;
+		var node = MemoizedTreeNode.create().nw;
 		node = node.setBit(-2,-2).setBit(-1,-2).setBit(0,-2).setBit(1,-2); //a 4x4 with the top four alive
 		var resultNode = node.slowSimulation(); //should be a 2x2 with the top two alive
 		this.assert(resultNode.population == 2);
@@ -90,14 +90,14 @@
 	});
 
 	test("nextGenerationTestEmpty",function(){
-		var node = TreeNodeBase.create();
+		var node = MemoizedTreeNode.create();
 		var next = node.nextGeneration();
 		this.assert(next.level == 2, "level should be 2");
 		this.assert(next.population == 0, "population should be 0");
 	});
 
 	test("nextGenerationTestNonEmpty",function(){
-		var node = TreeNodeBase.create().setBit(0,0).setBit(0,-1).setBit(0,1); //a blinker in its vertical state
+		var node = MemoizedTreeNode.create().setBit(0,0).setBit(0,-1).setBit(0,1); //a blinker in its vertical state
 		var next = node.nextGeneration();
 		this.assert(next.population == 3);
 		this.assert(next.getBit(0,0) == 1);
@@ -106,16 +106,16 @@
 	});
 
 	test("canonicalTreeNodeCreateTest",function(){
-		var created = CanonicalTreeNode.create();
+		var created = MemoizedTreeNode.create();
 		this.assert(created.nw == created.ne);
-		var createdAgain = CanonicalTreeNode.create();
+		var createdAgain = MemoizedTreeNode.create();
 		this.assert(created == createdAgain);
 		this.assert(created.nw == createdAgain.nw);
 	});
 
 	test("canonicalTreeNodeTest",function(){
-		var one = CanonicalTreeNode.create().setBit(0,0).setBit(0,1);
-		var two = CanonicalTreeNode.create().setBit(0,0).setBit(0,1);
+		var one = MemoizedTreeNode.create().setBit(0,0).setBit(0,1);
+		var two = MemoizedTreeNode.create().setBit(0,0).setBit(0,1);
 		this.assert(one == two);
 		this.assert(one.nw == two.nw);
 	});
