1ad0090d172186155ac9bc528c8b789affe2b5c3
Emile Fokkema <emilefokkema@gmail.com>
2017-10-11 19:09:07 +0200

    simplify usage of templates

--- scripts/topRightButtons.js
+++ scripts/topRightButtons.js
@@ -1,15 +1,12 @@
 define(["requireElement"],function(requireElement){
-	var topRightButtons = requireElement(document.getElementById("topRightButtons").innerHTML, function(container, buttonDiv){
+	var topRightButtons = requireElement(document.getElementById("topRightButtons"), function(buttonDiv){
 		var all = [];
 		var add = function(className, f){
 			buttonDiv(f, {className:className});
 		};
-		var attach = function(){
-			document.body.appendChild(container);
-		};
+		
 		return {
-			add:add,
-			attach:attach
+			add:add
 		};
 	});
 	return topRightButtons;
